1770a04a4ca9d63fb3ccdc51b05e994b5539ca5b
Yoonfoo <yoonfoo1102@gmail.com>
2024-12-08 21:36:06 +0800

    References to external CDN links for static assets

---
+++ src/app/projectLinks/links.ts
@@ -0,0 +1,40 @@
+type NestedObject = {
+    [key: string]: string | Record<string, string>
+}
+
+const Links: NestedObject = {
+    TEAM_LOGO : {
+        "ATL": "https://cdn.nba.com/logos/nba/streams/L/nss-team-atl.png",
+        "BOS": "https://cdn.nba.com/logos/nba/streams/L/nss-team-bos.png",
+        "BKN": "https://cdn.nba.com/logos/nba/streams/L/nss-team-bkn.png",
+        "CHA": "https://cdn.nba.com/logos/nba/streams/L/nss-team-cha.png",
+        "CHI": "https://cdn.nba.com/logos/nba/streams/L/nss-team-chi.png",
+        "CLE": "https://cdn.nba.com/logos/nba/streams/L/nss-team-cle.png",
+        "DAL": "https://cdn.nba.com/logos/nba/streams/L/nss-team-dal.png",
+        "DEN": "https://cdn.nba.com/logos/nba/streams/L/nss-team-den.png",
+        "DET": "https://cdn.nba.com/logos/nba/streams/L/nss-team-det.png",
+        "GSW": "https://cdn.nba.com/logos/nba/streams/L/nss-team-gsw.png",
+        "HOU": "https://cdn.nba.com/logos/nba/streams/L/nss-team-hou.png",
+        "IND": "https://cdn.nba.com/logos/nba/streams/L/nss-team-ind.png",
+        "LAC": "https://cdn.nba.com/logos/nba/streams/L/nss-team-lac.png",
+        "LAL": "https://cdn.nba.com/logos/nba/streams/L/nss-team-lal.png",
+        "MEM": "https://cdn.nba.com/logos/nba/streams/L/nss-team-mem.png",
+        "MIA": "https://cdn.nba.com/logos/nba/streams/L/nss-team-mia.png",
+        "MIL": "https://cdn.nba.com/logos/nba/streams/L/nss-team-mil.png",
+        "MIN": "https://cdn.nba.com/logos/nba/streams/L/nss-team-min.png",
+        "NOP": "https://cdn.nba.com/logos/nba/streams/L/nss-team-nop.png",
+        "NYK": "https://cdn.nba.com/logos/nba/streams/L/nss-team-nyk.png",
+        "OKC": "https://cdn.nba.com/logos/nba/streams/L/nss-team-okc.png",
+        "ORL": "https://cdn.nba.com/logos/nba/streams/L/nss-team-orl.png",
+        "PHI": "https://cdn.nba.com/logos/nba/streams/L/nss-team-phi.png",
+        "PHX": "https://cdn.nba.com/logos/nba/streams/L/nss-team-phx.png",
+        "POR": "https://cdn.nba.com/logos/nba/streams/L/nss-team-por.png",
+        "SAC": "https://cdn.nba.com/logos/nba/streams/L/nss-team-sac.png",
+        "SAS": "https://cdn.nba.com/logos/nba/streams/L/nss-team-sas.png",
+        "TOR": "https://cdn.nba.com/logos/nba/streams/L/nss-team-tor.png",
+        "UTA": "https://cdn.nba.com/logos/nba/streams/L/nss-team-uta.png",
+        "WAS": "https://cdn.nba.com/logos/nba/streams/L/nss-team-was.png"
+    }
+}
+
+export default Links
