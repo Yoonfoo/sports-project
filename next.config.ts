d45ec802e83cd305d81360cf998e997957800422
YoonFoo <yoonfoo1102@gmail.com>
2024-12-09 23:13:53 +0800

    Remote URL for teams logo added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.nba.com",
+        port: '',
+        pathname: "/logos/nba/streams/L/nss-team-**.png",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
