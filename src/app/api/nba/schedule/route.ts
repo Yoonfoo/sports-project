94f398daf0b8607c7e94a779f6d708737ff44375
YoonFoo <yoonfoo1102@gmail.com>
2024-12-09 23:15:55 +0800

    Remove unuse variable and library

--- src/app/api/nba/schedule/route.ts
+++ src/app/api/nba/schedule/route.ts
@@ -1,7 +1,5 @@
-import { NextRequest } from 'next/server';
-
-export async function GET(request: NextRequest) {
-    const res = await fetch('	https://cdn.nba.com/static/json/staticData/scheduleLeagueV2_32.json',{
+export async function GET() {
+    const res = await fetch('https://cdn.nba.com/static/json/staticData/scheduleLeagueV2_32.json',{
         headers:{
             "referer": "https://www.nba.com/",
         }
