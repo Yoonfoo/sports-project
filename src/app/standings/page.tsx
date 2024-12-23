f8b100dd7f9f272f068ae6120671b6e39785b12f
YoonFoo <yoonfoo1102@gmail.com>
2024-12-23 22:55:38 +0800

    Error fixed for deployment

--- src/app/standings/page.tsx
+++ src/app/standings/page.tsx
@@ -1,7 +1,7 @@
 import { teamStandings } from '../../../interface-definition/standings-type'
 import StandingMain from './standing-main';
 
-export async function getStandings() {
+async function getStandings() {
     const res = await fetch('https://stats.nba.com/stats/leaguestandingsv3?GroupBy=conf&LeagueID=00&Season=2024-25&SeasonType=Regular Season&Section=overall', {
       headers: {
         'referer': 'https://www.nba.com/',
