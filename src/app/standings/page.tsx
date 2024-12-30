eae2327faec4d6303cb1ef613554d875ead274f2
YoonFoo <yoonfoo1102@gmail.com>
2024-12-31 01:02:31 +0800

    Testing undergoing

--- src/app/standings/page.tsx
+++ src/app/standings/page.tsx
@@ -1,5 +1,5 @@
-import { teamStandings } from '../../../interface-definition/standings-type'
-import StandingMain from './standing-main';
+// import { teamStandings } from '../../../interface-definition/standings-type'
+// import StandingMain from './standing-main';
 
 // async function getStandings(): Promise<teamStandings> {
 //     const res = await fetch('https://stats.nba.com/stats/leaguestandingsv3?GroupBy=conf&LeagueID=00&Season=2024-25&SeasonType=Regular Season&Section=overall', {
