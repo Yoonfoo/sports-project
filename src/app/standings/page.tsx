e88ee4e71b07b6e386a2419fd9383b4b30b9089a
YoonFoo <yoonfoo1102@gmail.com>
2024-12-31 01:01:23 +0800

    Testing undergoing

--- src/app/standings/page.tsx
+++ src/app/standings/page.tsx
@@ -1,27 +1,27 @@
 import { teamStandings } from '../../../interface-definition/standings-type'
 import StandingMain from './standing-main';
 
-async function getStandings(): Promise<teamStandings> {
-    const res = await fetch('https://stats.nba.com/stats/leaguestandingsv3?GroupBy=conf&LeagueID=00&Season=2024-25&SeasonType=Regular Season&Section=overall', {
-      headers: {
-        'referer': 'https://www.nba.com/',
-      },
-    })
-    if(!res.ok) {
-        throw new Error('Failed to fetch standings')
-    }
-    const data = await res.json()
-    const standings = data.resultSets[0].rowSet
-    return standings
-}  
+// async function getStandings(): Promise<teamStandings> {
+//     const res = await fetch('https://stats.nba.com/stats/leaguestandingsv3?GroupBy=conf&LeagueID=00&Season=2024-25&SeasonType=Regular Season&Section=overall', {
+//       headers: {
+//         'referer': 'https://www.nba.com/',
+//       },
+//     })
+//     if(!res.ok) {
+//         throw new Error('Failed to fetch standings')
+//     }
+//     const data = await res.json()
+//     console.log(data)
+//     const standings = data.resultSets[0].rowSet
+//     return data
+// }  
 
 export default async function Standings() {
 
-    const standing = await getStandings()
-
     return (
         <div>
-            <StandingMain standings={standing}/>
+            {/* <StandingMain/> */}
+            {/* <StandingMain standings={standing}/> */}
         </div>
     );
 }
--- src/app/standings/standing-main.tsx
+++ src/app/standings/standing-main.tsx
@@ -1,16 +1,49 @@
 'use client'
 import Standing from './standing';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { teamStandings } from '../../../interface-definition/standings-type'
 
-type standingProps = {
-    standings: teamStandings
-}
+// type standingProps = {
+//     standings: teamStandings
+// }
 
-export default function StandingMain({standings}: standingProps) {
+async function getStandings(): Promise<teamStandings> {
+    const res = process.env.NODE_ENV === 'development'
+    ? await fetch(`http://localhost:3000/api/nba/standings`,{
+        headers:{
+            "referer": "http://www.nba.com/",
+        },
+        cache: "force-cache",
+    })
+    : await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/nba/standings`, {
+        headers:{
+            "referer": "http://www.nba.com/",
+        },
+        cache: "force-cache",
+    })
+    if(!res.ok) {
+        throw new Error('Failed to fetch standings')
+    }
+    const data = await res.json()
+    console.log(data)
+    const standings = data.resultSets[0].rowSet
+    return standings
+}   
+// {standings}: standingProps
+export default function StandingMain() {
 
     const [conferenceSelected, setConferenceSelected] = useState('East')
+    const [standings, setStandings] = useState<teamStandings>([])
 
+    useEffect(() => {
+        const fetchStandings = async () => {
+            const standings = await getStandings()
+            setStandings(standings)
+            console.log(standings)
+        }
+        fetchStandings()
+    },[])
+            
     return (
         <div className="flex flex-col w-full">
             <div className="bg-gray-300 flex flex-row justify-around h-16 w-full">
