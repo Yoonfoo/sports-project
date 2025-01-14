b66def0e8cfeb865a5a135a01b7a257fe379b288
Yoonfoo <yoonfoo1102@gmail.com>
2025-01-15 00:07:41 +0800

    standings fetch test

--- src/app/standings/page.tsx
+++ src/app/standings/page.tsx
@@ -21,19 +21,23 @@ import StandingMain from './standing-main';
 export default async function Standings() {
 
     const fetchStandings = async() => {
-        const apiUrl = process.env.NODE_ENV === 'development'
-        ?'http://localhost:3000/api/nba/standings'
-        :`${process.env.NEXT_PUBLIC_API_URL}/api/nba/standings`
-        const res = await fetch(apiUrl, {
+        // const apiUrl = process.env.NODE_ENV === 'development'
+        // ?'http://localhost:3000/api/nba/standings'
+        // :`${process.env.NEXT_PUBLIC_API_URL}/api/nba/standings`
+        const res = await fetch('https://stats.nba.com/stats/leaguestandingsv3?GroupBy=conf&LeagueID=00&Season=2024-25&SeasonType=Regular%20Season&Section=overall', {
+            headers: {
+                // 'host': 'stats.nba.com',
+                'referer': 'https://www.nba.com/',
+            },
             cache: "force-cache"
         })
         if(!res.ok) {
-            console.log(apiUrl)
-            console.log(process.env.NEXT_PUBLIC_API_URL)
+            console.log(res.status)
+            // console.log(process.env.NEXT_PUBLIC_API_URL)
             throw new Error('Failed to fetch standings')
         }
-        const standings = await res.json()
-        // const standings = data.resultSets[0].rowSet
+        const data = await res.json()
+        const standings = data.resultSets[0].rowSet
         return standings
     }
 
