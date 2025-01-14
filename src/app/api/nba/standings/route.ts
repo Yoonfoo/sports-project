9142787d7e1d31b1c89573ea8a04f39fa98092f4
Yoonfoo <yoonfoo1102@gmail.com>
2025-01-14 23:25:21 +0800

    standings fetch test

--- src/app/api/nba/standings/route.ts
+++ src/app/api/nba/standings/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-    const res = await fetch('https://stats.nba.com/stats/leaguestandingsv3?GroupBy=conf&LeagueID=00&Season=2024-25&SeasonType=Regular Season&Section=overall', {
+    const res = await fetch('https://stats.nba.com/stats/leaguestandingsv3?GroupBy=conf&LeagueID=00&Season=2024-25&SeasonType=Regular%20Season&Section=overall', {
         headers: {
             'host': 'stats.nba.com',
             'referer': 'https://www.nba.com/',
