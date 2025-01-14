d0766d3c9218b710b3cd1a1431cdffa466c545bb
Yoonfoo <yoonfoo1102@gmail.com>
2025-01-14 23:57:30 +0800

    standings fetch test

--- src/app/standings/page.tsx
+++ src/app/standings/page.tsx
@@ -1,26 +1,43 @@
-import { teamStandings } from '../../../interface-definition/standings-type'
+// import { teamStandings } from '../../../interface-definition/standings-type'
 import StandingMain from './standing-main';
 
-async function getStandings(): Promise<teamStandings> {
-    const apiUrl = process.env.NODE_ENV === 'development'
-    ?'http://localhost:3000/api/nba/standings'
-    :`${process.env.NEXT_PUBLIC_API_URL}/api/nba/standings`
-    const res = await fetch(apiUrl, {
-        cache: "force-cache"
-    })
-    if(!res.ok) {
-        console.log(apiUrl)
-        console.log(process.env.NEXT_PUBLIC_API_URL)
-        throw new Error('Failed to fetch standings')
-    }
-    const standings = await res.json()
-    // const standings = data.resultSets[0].rowSet
-    return standings
-}  
+// async function getStandings(): Promise<teamStandings> {
+//     const apiUrl = process.env.NODE_ENV === 'development'
+//     ?'http://localhost:3000/api/nba/standings'
+//     :`${process.env.NEXT_PUBLIC_API_URL}/api/nba/standings`
+//     const res = await fetch(apiUrl, {
+//         cache: "force-cache"
+//     })
+//     if(!res.ok) {
+//         console.log(apiUrl)
+//         console.log(process.env.NEXT_PUBLIC_API_URL)
+//         throw new Error('Failed to fetch standings')
+//     }
+//     const standings = await res.json()
+//     // const standings = data.resultSets[0].rowSet
+//     return standings
+// }  
 
 export default async function Standings() {
 
-    const standing = await getStandings()
+    const fetchStandings = async() => {
+        const apiUrl = process.env.NODE_ENV === 'development'
+        ?'http://localhost:3000/api/nba/standings'
+        :`${process.env.NEXT_PUBLIC_API_URL}/api/nba/standings`
+        const res = await fetch(apiUrl, {
+            cache: "force-cache"
+        })
+        if(!res.ok) {
+            console.log(apiUrl)
+            console.log(process.env.NEXT_PUBLIC_API_URL)
+            throw new Error('Failed to fetch standings')
+        }
+        const standings = await res.json()
+        // const standings = data.resultSets[0].rowSet
+        return standings
+    }
+
+    const standing = await fetchStandings()
 
     return (
         <div>
