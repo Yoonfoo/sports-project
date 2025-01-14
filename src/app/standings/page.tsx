f9df49f94ddac41743e8932993ec616172b9c88d
Yoonfoo <yoonfoo1102@gmail.com>
2025-01-14 23:33:09 +0800

    standings fetch test

--- src/app/standings/page.tsx
+++ src/app/standings/page.tsx
@@ -1,21 +1,34 @@
 import { teamStandings } from '../../../interface-definition/standings-type'
 import StandingMain from './standing-main';
 
-async function getStandings(): Promise<teamStandings> {
-    const res = process.env.NODE_ENV === 'development'
-    ?await fetch('http://localhost:3000/api/nba/standings')
-    :await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/nba/standings`)
-    if(!res.ok) {
-        console.log(res.status)
-        throw new Error('Failed to fetch standings')
-    }
-    const standings = await res.json()
-    // const standings = data.resultSets[0].rowSet
-    return standings
-}  
+// async function getStandings(): Promise<teamStandings> {
+//     const res = process.env.NODE_ENV === 'development'
+//     ?await fetch('http://localhost:3000/api/nba/standings')
+//     :await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/nba/standings`)
+//     if(!res.ok) {
+//         console.log(res.status)
+//         throw new Error('Failed to fetch standings')
+//     }
+//     const standings = await res.json()
+//     // const standings = data.resultSets[0].rowSet
+//     return standings
+// }  
 
 export default async function Standings() {
 
+    
+    async function getStandings(): Promise<teamStandings> {
+        const res = process.env.NODE_ENV === 'development'
+        ?await fetch('http://localhost:3000/api/nba/standings')
+        :await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/nba/standings`)
+        if(!res.ok) {
+            console.log(res.status)
+            throw new Error('Failed to fetch standings')
+        }
+        const standings = await res.json()
+        // const standings = data.resultSets[0].rowSet
+        return standings
+    }  
     const standing = await getStandings()
 
     return (
