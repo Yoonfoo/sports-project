8c932bd9382c689317f5fccbc8d8f164f875e34e
YoonFoo <yoonfoo1102@gmail.com>
2025-03-06 17:07:23 +0800

    Bug fixed

--- src/app/api/nba/standings/route.ts
+++ src/app/api/nba/standings/route.ts
@@ -2,7 +2,7 @@ import { teamStandings } from "../../../../../interface-definition/standings-typ
 
 export async function GET() {
     const res = await fetch('https://stats.nba.com/stats/leaguestandingsv3?GroupBy=conf&LeagueID=00&Season=2024-25&SeasonType=Regular%20Season&Section=overall', {
-        method: 'GET',
+        // method: 'GET',
         headers: {
             // 'host': 'stats.nba.com',
             'Origin': 'https://www.nba.com',
