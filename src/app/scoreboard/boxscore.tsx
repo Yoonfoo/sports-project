de7c4771bf05cf140b03b77c7679fda2508b2777
Yoonfoo <yoonfoo1102@gmail.com>
2025-04-28 15:23:24 +0800

    Layout bug fixed

--- src/app/scoreboard/boxscore.tsx
+++ src/app/scoreboard/boxscore.tsx
@@ -19,7 +19,7 @@ const BoxScore: FC<BoxScoreProps> = ({boxscore ,teamLogos}) => {
 
     return (
 
-    <div className="overflow-auto lg:mx-96 lg:py-8 lg:border-2 lg:border-gray-100 lg:rounded-2xl lg:my-4">
+    <div className="overflow-auto lg:mx-80 lg:py-8 lg:border-2 lg:border-gray-100 lg:rounded-2xl lg:my-4">
         <div>
 
             <div className="my-4 lg:my-0">
@@ -109,7 +109,7 @@ const BoxScore: FC<BoxScoreProps> = ({boxscore ,teamLogos}) => {
                     <tbody>
                         {selectTeamBoxScore.players.map((player) => (
                             <tr key={player.personId} className="even:bg-slate-100 odd:bg-white">
-                                <td className="px-2 py-2 sticky left-0 bg-white border-r-2 border-gray-100 lg:bg-transparent">
+                                <td className="px-2 py-2 sticky left-0 bg-white border-r-2 border-gray-100 lg:bg-transparent lg:w-80">
                                     {window.innerWidth > 1024
                                     ? player.name
                                     : player.name.split(" ")[0][0] + ". "  + player.name.split(" ").slice(1)
